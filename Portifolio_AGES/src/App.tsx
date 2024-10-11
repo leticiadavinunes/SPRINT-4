f457963a6a3c0f9b017ff979ce4c9de7ee151193
Leticia Davi <leticia.davi@edu.pucrs.br>
2024-10-11 14:52:16 -0300

    Não sei

--- Portifolio_AGES/src/App.tsx
+++ Portifolio_AGES/src/App.tsx
@@ -1,41 +1,41 @@
-import './App.css'
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./home";
+import AboutMe from "./AboutMe";
+import Contacts from "./contacts";
+import "./App.css";
+import Projects from "./projects";
 
-function App() {
-  
+const App: React.FC = () => {
   return (
     <Router>
+      <div className="BarraNavegador_C">
+        <nav className="BarraNavegador">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
 
-        <div>
-            <nav>
-              <ul>
-                  <li>
-                    <Link to="/">Home</Link>
-                  </li>
-                  <li>
-                    <Link to="/aboutMe">About me</Link>
-                  </li>
-                  <li>
-                    <Link to="/projects">Projects</Link>
-                  </li>
-                  <li>
-                    <Link to="/contacts" >Contacts</Link>
-                  </li>
-              </ul>
-            </nav>
-
-            <Routes>
-                <Route path='/' element={<Home />}/>
-                <Route path='/aboutMe' element={<AboutMe />}/>
-                <Route path='/projects' element={<Projects />}/>
-                <Route path='/contacts' element={<Contacts/>}/>
-            </Routes>
-        </div>
-    
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutMe />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contacts />} />
+        </Routes>
+      </div>
     </Router>
-  )
-  
-}
+  );
+};
 
-export default App
+export default App;
---
+++ Portifolio_AGES/src/Contacts.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Contacts() {
+  return <h2>Contacts</h2>;
+}
+
+
+export default Contacts;
