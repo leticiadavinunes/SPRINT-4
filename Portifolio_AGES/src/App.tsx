7603375cb54eacd8a5e442ca8724b6663418dead
Leticia Davi <leticia.davi@edu.pucrs.br>
2024-10-15 14:31:08 -0300

    Teste

--- Portifolio_AGES/src/App.tsx
+++ Portifolio_AGES/src/App.tsx
@@ -1,32 +1,33 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import Home from "./home";
+import Home from "./Home";
 import AboutMe from "./AboutMe";
-import Contacts from "./contacts";
+import Contacts from "./Contacts";
 import "./App.css";
-import Projects from "./projects";
+import Projects from "./Projects";
 
 const App: React.FC = () => {
   return (
     <Router>
       <div className="BarraNavegador_C">
-        <nav className="BarraNavegador">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/projects">Projects</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-
+        <div className="Teste">
+          <nav className="BarraNavegador">
+            <ul className="Links">
+              <li>
+                <Link to="/">Home|</Link>
+              </li>
+              <li>
+                <Link to="/about">About Me|</Link>
+              </li>
+              <li>
+                <Link to="/projects">Projects|</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact|</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<AboutMe />} />
